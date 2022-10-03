20e9a89d5ae79b42c7b74102a13f37a05e3bbbcf
Pushpakkumar <57779932+PushpakkumarBH@users.noreply.github.com>
2022-10-03 10:06:44 +0530

    modified some changes in header

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+// const nodemailer = require('nodemailer');
+
 const app = express();
 app.set('view engine', 'ejs' );
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-// mongoose.connect("mongodb+srv://<Username>:<Password>@cluster1.mictlsi.mongodb.net/IeeeDB");
+mongoose.connect("mongodb+srv://pushpak696:S8H4zXQ8eq01ah1X@cluster1.mictlsi.mongodb.net/IeeeDB");
 
-mongoose.connect("mongodb://localhost:27017/IeeeDB", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb://localhost:27017/IeeeDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', function(req, res){
     res.render('home');
@@ -27,6 +29,36 @@ const participantSchema = new mongoose.Schema ({
 
 const Participant = mongoose.model("Participant", participantSchema);
 
+// nodemailer functionality
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'bahubalish1974@gmail.com',
+//         pass: 'HiTTalkeri11,',
+//     }
+// });
+
+// const mailOptions = {
+//     from: 'bahubalish1974@gmail.com',
+//     to: 'pushpak696@gmail.com',
+//     subject: 'Sending Email using Node.js',
+//     text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+// config.action_mailer.smtp_settings = {
+//     password: 'zlcaavhusckzwgzr',
+//     authentication: 'plain',
+//     enable_starttls_auto: true
+//   }
+
+
 app.get('/ieeeregister', function(req, res){
     res.render('ieeeregister');
 });
@@ -42,10 +74,24 @@ app.post("/ieeeregister", function(req, res){
         event: req.body.event
 
     });
+    // const mailOptions = {
+    //     from: 'bahubalish1974@gmail.com',
+    //     to: newParticipant.email,
+    //     subject: 'Sucessfully Registered',
+    //     text: 'Thank you for registering for the event visit our website for more details about event...'
+    // };
     newParticipant.save(function(err){
         if(err){
             console.log(err);
         } else {
+            // transporter.sendMail(mailOptions, function(error, info){
+            //     if (error) {
+            //         console.log(error);
+            //     } else {
+            //         console.log('Email sent: ' + info.response);
+        
+            //     }
+            // });
             res.redirect('/');
         }
     });

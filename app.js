676c14c93db142686288fd934a22a83ce733ce59
Pushpakkumar <57779932+PushpakkumarBH@users.noreply.github.com>
2022-10-09 13:48:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('dotenv').config()
 
 // mongoose.connect("mongodb+srv://pushpak696:"+process.env.MONGODB_PWD+"@cluster1.mictlsi.mongodb.net/IeeeDB");
 
-// mongoose.connect("mongodb://localhost:27017/IeeeDB", {useNewUrlParser: true, useUnifiedTopology: true});
+ mongoose.connect("mongodb://localhost:27017/IeeeDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', function(req, res){
     res.render('home');
